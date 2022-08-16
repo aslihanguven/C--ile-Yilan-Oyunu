c192117582930327fa031dc30c83fe3370ffcb02
aslihanguven <98274590+aslihanguven@users.noreply.github.com>
2022-08-16 13:00:52 +0300

    C# ile Yılan Oyunu

---
+++ C# ile Yılan Oyunu/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Fare_Button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.yilanHareket = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.timerEkle = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SandyBrown;
+            this.button1.Enabled = false;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(232, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // yilanHareket
+            // 
+            this.yilanHareket.Enabled = true;
+            this.yilanHareket.Interval = 180;
+            this.yilanHareket.Tick += new System.EventHandler(this.yilanHareket_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PeachPuff;
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(262, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PeachPuff;
+            this.button3.Enabled = false;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(292, 245);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PeachPuff;
+            this.button4.Enabled = false;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(322, 245);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 30);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.PeachPuff;
+            this.button5.Enabled = false;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(352, 245);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(30, 30);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.PeachPuff;
+            this.button6.Enabled = false;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(382, 245);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(30, 30);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.PeachPuff;
+            this.button7.Enabled = false;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(412, 245);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(30, 30);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.PeachPuff;
+            this.button8.Enabled = false;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(442, 245);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(30, 30);
+            this.button8.TabIndex = 0;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timerEkle
+            // 
+            this.timerEkle.Enabled = true;
+            this.timerEkle.Interval = 10000;
+            this.timerEkle.Tick += new System.EventHandler(this.timerEkle_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(649, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "8";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(704, 521);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yılan Oyunu - Mesut EROL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer yilanHareket;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Timer timerEkle;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C# ile Yılan Oyunu/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fare_Button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string hedefYonu = "";
+        int atla = 30;
+        List<Point> konumListesi = new List<Point>();
+        int btnSayisi = 8;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void yilanHareket_Tick(object sender, EventArgs e)
+        {
+
+            if (hedefYonu == "L")
+            {
+                konumListesi.Add(new Point(button1.Location.X - atla, button1.Location.Y));
+            }
+            else if (hedefYonu == "U")
+            {
+                konumListesi.Add(new Point(button1.Location.X, button1.Location.Y - atla));
+            }
+            else if (hedefYonu == "R")
+            {
+                konumListesi.Add(new Point(button1.Location.X + atla, button1.Location.Y));
+            }
+            else if (hedefYonu == "D")
+            {
+                konumListesi.Add(new Point(button1.Location.X, button1.Location.Y + atla));
+            }
+
+            yilaniKonumlandir();
+
+        }
+
+        private void yilaniKonumlandir()
+        {
+
+            //Yılanı konuma göre hareketlendir
+            for (int i = 1; i <= btnSayisi; i++)
+            {
+                Button btn = this.Controls["button" + i] as Button;
+                btn.Location = konumListesi[konumListesi.Count - i];
+            }
+
+
+            //Duvara çarptığında çık
+            if (button1.Location.X < 0 || button1.Location.X > this.Width)
+            {
+                Environment.Exit(0);
+            }
+            else if (button1.Location.Y < 0 || button1.Location.Y > this.Height)
+            {
+                Environment.Exit(0);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hedefYonu = "L";
+            for (int i = btnSayisi; i >= 1; i--)
+            {
+                Button btn = this.Controls["button" + i] as Button;
+                konumListesi.Add(new Point(btn.Location.X, btn.Location.Y));
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyData == Keys.Left)
+            {
+                hedefYonu = "L";
+                konumListesi.Add(new Point(button1.Location.X - atla, button1.Location.Y));
+            }
+            else if (e.KeyData == Keys.Up)
+            {
+                hedefYonu = "U";
+                konumListesi.Add(new Point(button1.Location.X, button1.Location.Y - atla));
+            }
+            else if (e.KeyData == Keys.Right)
+            {
+                hedefYonu = "R";
+                konumListesi.Add(new Point(button1.Location.X + atla, button1.Location.Y));
+            }
+            else if (e.KeyData == Keys.Down)
+            {
+                hedefYonu = "D";
+                konumListesi.Add(new Point(button1.Location.X, button1.Location.Y + atla));
+            }
+
+            yilaniKonumlandir();
+        }
+
+        private void timerEkle_Tick(object sender, EventArgs e)
+        {
+
+            Button btnSon = this.Controls["button" + btnSayisi] as Button;
+
+            btnSayisi++;
+            Button btn = new Button();
+            btn.Name = "button" + btnSayisi;
+            btn.Text = "";
+            btn.Size = new Size(atla, atla);
+            btn.BackColor = button8.BackColor;
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.FlatAppearance.BorderColor = button8.FlatAppearance.BorderColor;
+            btn.FlatAppearance.BorderSize = button8.FlatAppearance.BorderSize;
+            btn.Enabled = false;
+
+            if (hedefYonu == "L")
+            {
+                btn.Location = new Point(btnSon.Location.X + atla, btnSon.Location.Y);
+            }
+            else if (hedefYonu == "U")
+            {
+                btn.Location = new Point(btnSon.Location.X, btnSon.Location.Y + atla);
+            }
+            else if (hedefYonu == "R")
+            {
+                btn.Location = new Point(btnSon.Location.X + -atla, btnSon.Location.Y);
+            }
+            else if (hedefYonu == "D")
+            {
+                btn.Location = new Point(btnSon.Location.X, btnSon.Location.Y - atla);
+            }
+
+            this.Controls.Add(btn);
+            label1.Text = btnSayisi.ToString();
+        }
+    }
+}
